460c07e587ea348bd83b027bad00b02fce69963a
DESKTOP-DPDSLQD\JinUk <jin960828@naver.com>
2025-06-30 19:29:09 +0900

    correct event list, reservation, detail, datepicker, add ticket table logic

--- src/wrapper/ClientAuthWrapper.tsx
+++ src/wrapper/ClientAuthWrapper.tsx
@@ -1,12 +1,12 @@
 'use client';
 
-import { usePathname, useRouter } from 'next/navigation';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import { useSession } from '@/hooks/useSession';
 import useToast from '@/hooks/useToast';
 import { useSpinner } from '@/providers/SpinnerProvider'; // 경로는 맞게 수정
 
-const publicPaths = ['/login', '/register', '/auth/find', '/auth/callback', '/auth/reset-password', '/events'];
+const publicPaths = ['/login', '/register', '/auth/find', '/auth/callback', '/auth/reset-password'];
 
 const ClientAuthWrapper = ({ children }: { children: React.ReactNode }) => {
 	const pathname = usePathname();
@@ -14,24 +14,28 @@ const ClientAuthWrapper = ({ children }: { children: React.ReactNode }) => {
 	const { loading, isLoggedIn } = useSession();
 	const { showToast } = useToast();
 	const { showSpinner, hideSpinner } = useSpinner();
+	const searchParams = useSearchParams();
+	const isLoggedOut = searchParams.get('loggedOut') === '1';
 
 	const [ready, setReady] = useState(false);
 
 	useEffect(() => {
 		if (!loading) {
 			if (!isLoggedIn && !publicPaths.includes(pathname)) {
-				showSpinner(true);
-				showToast({
-					message: '비정상 접근이에요. 로그인 화면으로 이동합니다.',
-					iconType: 'error',
-					autoCloseTime: 3000,
-				});
+				if (!isLoggedOut) {
+					showSpinner(true);
+					showToast({
+						message: '비정상 접근이에요. 로그인 화면으로 이동합니다.',
+						iconType: 'error',
+						autoCloseTime: 3000,
+					});
+				}
 				router.push('/login');
 			} else {
 				setReady(true);
 			}
 		}
-	}, [loading, isLoggedIn, pathname, router, showSpinner, showToast]);
+	}, [loading, isLoggedIn, pathname, router, showSpinner, showToast, isLoggedOut]);
 
 	useEffect(() => {
 		if (ready) {
